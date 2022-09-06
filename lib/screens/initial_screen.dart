2110872ddd190b5f748c189c6b1000640c19bd90
K4l1B3 <luizhlimagomes28@gmail.com>
2022-09-05 23:26:04 -0300

    first commit

---
+++ lib/screens/initial_screen.dart
@@ -0,0 +1,80 @@
+
+import 'package:flutter/material.dart';
+import 'package:nosso_primeiro_projeto/components/task.dart';
+
+class InitialScreen extends StatefulWidget {
+
+  const InitialScreen({Key? key}) : super(key: key);
+
+  @override
+  State<InitialScreen> createState() => _InitialScreenState();
+}
+
+class _InitialScreenState extends State<InitialScreen> {
+  bool opacidade = true;
+  ValueNotifier<bool> isDialOpen = ValueNotifier(false);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Container(),
+        title: const Text('Tarefas'),
+      ),
+      body: AnimatedOpacity(
+        opacity: opacidade ? 1 : 0,
+        duration: const Duration(milliseconds: 800),
+        child: ListView(
+          children: const [
+            Task(
+                'Aprender Flutter',
+                'assets/images/dash.png',
+                3),
+            Task(
+                'Andar de Bike',
+                'assets/images/bike.webp',
+                2),
+            Task(
+                'Meditar',
+                'assets/images/meditar.jpeg',
+                5),
+            Task(
+                'Ler',
+                'assets/images/livro.jpg',
+                4),
+            Task(
+              'Jogar',
+              'assets/images/jogar.jpg',
+              1,
+            ),
+            SizedBox(height: 80,)
+          ],
+        ),
+      ),
+
+
+
+        // floatingActionButton: Row (
+        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     FloatingActionButton(
+        //       onPressed: () {
+        //         setState(() {
+        //           opacidade = !opacidade;
+        //         });
+        //       },
+        //       child: const Icon(Icons.remove_red_eye),
+        //     ),
+        //     FloatingActionButton(
+        //       onPressed: () {
+        //         Navigator.pushNamed(context, '/form-screen');
+        //       },
+        //       child:  const Icon(Icons.add),
+        //     ),
+        //   ],
+        // )
+
+    );
+  }
+}
